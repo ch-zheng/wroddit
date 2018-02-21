27734f42e5017a6f6350f8732420384b789b8a16
Charlie Zheng <chzheng.2@gmail.com>
2018-02-20 18:11:54 -0600

    Second commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 //Index.js
+'use strict';
+const express = require('express');
+const path = require('path');
+const server = express();
+
+server.set('view engine', 'ejs');
+server.get('/', (req, res) => res.send('Hello World!'));
+server.use('/w/frankenstein', express.static(path.join(__dirname, 'public'), {index: 'frankenstein.html'}));
+server.listen(3000, () => console.log('Listening on port 3000'));
