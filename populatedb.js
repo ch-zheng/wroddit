83c583866ad06deb3b572aa272bb4133ec4c3039
Hang Sun <sun.hang2000@gmail.com>
2018-02-23 20:41:55 -0600

    change populatedb script

--- populatedb.js
+++ populatedb.js
@@ -1,7 +1,7 @@
 const User = require('./schemas/user.js');
 const Comment = require('./schemas/comment.js');
 const Post = require('./schemas/post.js');
-
+const argon2 = require('argon2');
 const mongoose = require('mongoose');
 const mongodb = 'mongodb://crack:crack@ds243728.mlab.com:43728/wroddit';
 mongoose.connect(mongodb);
@@ -15,12 +15,12 @@ let comments = [];
 async function createUser(username, passwd) {
     user = {
         username: username,
-        hash: passwd,
+        hash: await argon2.hash(passwd),
     };
 
     const newUser = new User(user);
     await newUser.save();
-    console.log(`New User: ${user.username}`);
+    console.log(`New User: ${user.username} with hash: ${user.hash}`);
     users.push(newUser);
 }
 
@@ -41,17 +41,13 @@ async function createPost(title, content, user, kind = 'text') {
 createUser('hang', 'f')
     .then(_ => createUser('charlie', 'f'))
     .then(_ => createUser('ethan', 'f'))
+    .then(_ => createUser('Vladimir Vodkov', 'f'))
+    .then(_ => createUser('Natasha Nizamutdinova', 'f'))
+    .then(_ => createUser('Dmitri Dmitrijev', 'f'))
+    .then(_ => createUser('Boris Blokov', 'f'))
+    .then(_ => createUser('Crowley', 'password'))
+    .then(_ => createUser('Boylan', 'proveit'))
     .then(_ => createPost('filler', 'filler', users[0]._id))
     .then(_ => createPost('filler', 'filler', users[1]._id));
-
-// createUser('hang', 'hang');
-// createUser('charlie', 'charlie');
-// createUser('ethan', 'ethan');
-// setTimeout(() => {
-//     createPost('hello', 'test content!', users[0]._id);
-//     createPost('hi', 'test content!', users[1]._id);
-//     createPost('hola', 'test content!', users[2]._id);
-// }, 2000)
-
 
 
